24d77cd7d2bf8117bede5fc65c261ebc6aad72ba
Azorkss <loic.costiou@gmail.com>
2025-10-21 11:06:27 +0200

    test mouchard reussi

--- test/test_mouchard.cpp
+++ test/test_mouchard.cpp
@@ -7,27 +7,27 @@ int main(void) {
  complexe_t::stats();
 
  std::cout << std::endl << "complexe_t c1(1,2);" << std::endl;
- complexe_t c1(1,2);
+ complexe_t c1(1,2); //Constructeur ad hoc
  complexe_t::stats();
 
  std::cout << std::endl << "complexe_t c2(3,4);" << std::endl;
- complexe_t c2(3,4);
+ complexe_t c2(3,4); //Constructeur ad hoc
  complexe_t::stats();
 
  std::cout << std::endl << "complexe_t c3(c1);" << std::endl;
- complexe_t c3(c1);
+ complexe_t c3(c1); // Constructeur copie
  complexe_t::stats();
 
  std::cout << std::endl << "complexe_t c4(std::move(c2));" << std::endl;
- complexe_t c4(std::move(c2));
+ complexe_t c4(std::move(c2)); // Constructeur mouvement
  complexe_t::stats();
 
  std::cout << std::endl << "c3=c1;" << std::endl;
- c3=c1;
+ c3=c1; // Affectation copie
  complexe_t::stats();
 
  std::cout << std::endl << "c4=complexe_t(5,6);" << std::endl;
- c4=complexe_t(5,6);
+ c4=complexe_t(5,6); // Constructeur had oc puis Affectation par mouvement
  complexe_t::stats();
 
  return 0;
